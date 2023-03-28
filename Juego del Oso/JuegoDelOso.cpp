e7b634dad7e104cf7c4d31c9f55ccab56e5cafee
Nicolas Quintanilla <107016019+Niclaii@users.noreply.github.com>
2023-03-28 11:07:24 -0500

    Add files via upload

--- Juego del Oso/JuegoDelOso.cpp
+++ Juego del Oso/JuegoDelOso.cpp
@@ -2,19 +2,24 @@
 
 using namespace std;
 
-int main() 
+int main()
 {
-	char F1C1{}, F1C2{}, F1C3{}, F1C4{}, F1C5{}, F2C1{}, F2C2{}, F2C3{}, F2C4{}, F2C5{}, F3C1{}, F3C2{},F3C3{}, F3C4{}, F3C5{}, F4C1{}, F4C2{}, F4C3{}, F4C4{}, F4C5{}, F5C1{}, F5C2{}, F5C3{},F5C4{}, F5C5{};
-	int ubi{0}, i{0};
+	char F1C1{}, F1C2{}, F1C3{}, F1C4{}, F1C5{}, F2C1{}, F2C2{}, F2C3{}, F2C4{}, F2C5{}, F3C1{}, F3C2{}, F3C3{}, F3C4{}, F3C5{}, F4C1{}, F4C2{}, F4C3{}, F4C4{}, F4C5{}, F5C1{}, F5C2{}, F5C3{}, F5C4{}, F5C5{};
+	int i{ 0 };
+	float ubi{ 0 };
 	bool primerapasada{ true }, jugador1{ true }, jugador2{ false }, tmp{};
-	char s{'S'}, o{'O'};
-	
+	char s{ 'S' }, o{ 'O' };
+
 	do
 	{
 		//PRIMERA PASADA
 		if (primerapasada)
 		{
-			cout << '7' << " | " << '8' << " | " << '9' << "\n" << '4' << " | " << '5' << " | " << '6' << "\n" << '1' << " | " << '2' << " | " << '3' << "\n";
+			cout << "(1, 1)" << " | " << "(1, 2)" << " | " << "(1, 3)" << " | " << "(1, 4)" << " | " << "(1, 5)"<< " \n "<<"------------------------------------------" <<"\n"
+				 << "(2, 1)" << " | " << "(2, 2)" << " | " << "(2, 3)" << " | " << "(2, 4)" << " | " << "(2, 5)" << " \n " << "------------------------------------------" << "\n" 
+				 << "(3, 1)" << " | " << "(3, 2)" << " | " << "(3, 3)" << " | " << "(3, 4)" << " | " << "(3, 5)" << " \n " << "------------------------------------------" << "\n"
+				 << "(4, 1)" << " | " << "(4, 2)" << " | " << "(4, 3)" << " | " << "(4, 4)" << " | " << "(4, 5)" << " \n " << "------------------------------------------" << "\n"
+				 << "(5, 1)" << " | " << "(5, 2)" << " | " << "(5, 3)" << " | " << "(5, 4)" << " | " << "(5, 5)" << endl;
 			primerapasada = false;
 		}
 		//TURNOS
@@ -35,162 +40,378 @@ int main()
 
 		if (jugador1)
 		{
-			if (ubi == 1)
+			if (ubi == 1,1)
 			{
 				if (F1C1 != s || F1C1 != o)
 				{
 					F1C1 = s;
 				}
 			}
-			else if (ubi == 2)
+			else if (ubi == 1,2)
 			{
 				if (F1C2 != s || F1C2 != o)
 				{
 					F1C2 = s;
 				}
 			}
-			else if (ubi == 3)
+			else if (ubi == 1,3)
 			{
 				if (F1C3 != s || F1C3 != o)
 				{
 					F1C3 = s;
 				}
 			}
-			else if (ubi == 4)
+			else if (ubi == 1,4)
 			{
 				if (F1C4 != s || F1C4 != o)
 				{
 					F1C4 = s;
 				}
 			}
-			else if (ubi == 5)
+			else if (ubi == 1,5)
 			{
 				if (F1C5 != s || F1C5 != o)
 				{
 					F1C5 = s;
 				}
 			}
-			else if (ubi == 6)
+			else if (ubi == 2,1)
 			{
 				if (F2C1 != s || F2C1 != o)
 				{
 					F2C1 = s;
 				}
 			}
-			else if (ubi == 7)
+			else if (ubi == 2,2)
 			{
 				if (F2C2 != s || F2C2 != o)
 				{
 					F2C2 = s;
 				}
 			}
-			else if (ubi == 8)
+			else if (ubi == 2,3)
 			{
-				if (F8 != s || F8 != o)
+				if (F2C3 != s || F2C3 != o)
 				{
-					F8 = s;
+					F2C3 = s;
 				}
 			}
-			else if (ubi == 9)
+			else if (ubi == 2,4)
 			{
-				if (F9 != s || F9 != o)
+				if (F2C4 != s || F2C4 != o)
 				{
-					F9 = s;
+					F2C4 = s;
+				}
+			}
+			else if (ubi == 2,5)
+			{
+				if (F2C5 != s || F2C5 != o)
+				{
+					F2C5 = s;
+				}
+			}
+			else if (ubi == 3,1)
+			{
+				if (F3C1 != s || F3C1 != o)
+				{
+					F3C1 = s;
+				}
+			}
+			else if (ubi == 3,2)
+			{
+				if (F3C2 != s || F3C2 != o)
+				{
+					F3C2 = s;
+				}
+			}
+			else if (ubi == 3,3)
+			{
+				if (F3C3 != s || F3C3 != o)
+				{
+					F3C3 = s;
+				}
+			}
+			else if (ubi == 3,4)
+			{
+				if (F3C4 != s || F3C4 != o)
+				{
+					F3C4 = s;
+				}
+			}
+			else if (ubi == 3,5)
+			{
+				if (F3C5 != s || F3C5 != o)
+				{
+					F3C5 = s;
+				}
+			}
+			else if (ubi == 4,1)
+			{
+				if (F4C1 != s || F4C1 != o)
+				{
+					F4C1 = s;
+				}
+			}
+			else if (ubi == 4,2)
+			{
+				if (F4C2 != s || F4C2 != o)
+				{
+					F4C2 = s;
+				}
+			}
+			else if (ubi == 4,3)
+			{
+				if (F4C3 != s || F4C3 != o)
+				{
+					F4C3 = s;
+				}
+			}
+			else if (ubi == 4,4)
+			{
+				if (F4C4 != s || F4C4 != o)
+				{
+					F4C4 = s;
+				}
+			}
+			else if (ubi == 4,5)
+			{
+				if (F4C5 != s || F4C5 != o)
+				{
+					F4C5 = s;
+				}
+			}
+			else if (ubi == 5,1)
+			{
+				if (F5C1 != s || F5C1 != o)
+				{
+					F5C1 = s;
+				}
+			}
+			else if (ubi == 5,2)
+			{
+				if (F5C2 != s || F5C2 != o)
+				{
+					F5C2 = s;
+				}
+			}
+			else if (ubi == 5,3)
+			{
+				if (F5C3 != s || F5C3 != o)
+				{
+					F5C3 = s;
+				}
+			}
+			else if (ubi == 5,4)
+			{
+				if (F5C4 != s || F5C4 != o)
+				{
+					F5C4 = s;
+				}
+			}
+			else if (ubi == 5,5)
+			{
+				if (F5C5 != s || F5C5 != o)
+				{
+					F5C5 = s;
 				}
 			}
 		}
 		//JUGADOR 2
 		if (jugador2)
 		{
-			switch (ubi)
+			if (ubi == 1, 1)
 			{
-			case 1:
-			{
-				if (F1 != s || F1 != o)
+				if (F1C1 != s || F1C1 != o)
 				{
-					F1 = o;
-					break;
+					F1C1 = o;
 				}
 			}
-			case 2:
+			else if (ubi == 1, 2)
 			{
-				if (F2 != s || F2 != o)
+				if (F1C2 != s || F1C2 != o)
 				{
-					F2 = o;
-					break;
+					F1C2 = o;
 				}
 			}
-			case 3:
+			else if (ubi == 1, 3)
 			{
-				if (F3 != s || F3 != o)
+				if (F1C3 != s || F1C3 != o)
 				{
-					F3 = o;
-					break;
+					F1C3 = o;
 				}
 			}
-			case 4:
+			else if (ubi == 1, 4)
 			{
-				if (F4 != s || F4 != o)
+				if (F1C4 != s || F1C4 != o)
 				{
-					F4 = o;
-					break;
+					F1C4 = o;
 				}
 			}
-			case 5:
+			else if (ubi == 1, 5)
 			{
-				if (F5 != s || F5 != o)
+				if (F1C5 != s || F1C5 != o)
 				{
-					F5 = o;
-					break;
+					F1C5 = o;
 				}
 			}
-			case 6:
+			else if (ubi == 2, 1)
 			{
-				if (F6 != s || F6 != o)
+				if (F2C1 != s || F2C1 != o)
 				{
-					F6 = o;
-					break;
+					F2C1 = o;
 				}
 			}
-			case 7:
+			else if (ubi == 2, 2)
 			{
-				if (F7 != s || F7 != o)
+				if (F2C2 != s || F2C2 != o)
 				{
-					F7 = o;
-					break;
+					F2C2 = o;
 				}
 			}
-			case 8:
+			else if (ubi == 2, 3)
 			{
-				if (F8 != s || F8 != o)
+				if (F2C3 != s || F2C3 != o)
 				{
-					F8 = o;
-					break;
+					F2C3 = o;
 				}
 			}
-			case 9:
+			else if (ubi == 2, 4)
 			{
-				if (F9 != s || F9 != o)
+				if (F2C4 != s || F2C4 != o)
 				{
-					F9 = o;
-					break;
+					F2C4 = o;
 				}
 			}
-
+			else if (ubi == 2, 5)
+			{
+				if (F2C5 != s || F2C5 != o)
+				{
+					F2C5 = o;
+				}
+			}
+			else if (ubi == 3, 1)
+			{
+				if (F3C1 != s || F3C1 != o)
+				{
+					F3C1 = o;
+				}
+			}
+			else if (ubi == 3, 2)
+			{
+				if (F3C2 != s || F3C2 != o)
+				{
+					F3C2 = o;
+				}
+			}
+			else if (ubi == 3, 3)
+			{
+				if (F3C3 != s || F3C3 != o)
+				{
+					F3C3 = o;
+				}
+			}
+			else if (ubi == 3, 4)
+			{
+				if (F3C4 != s || F3C4 != o)
+				{
+					F3C4 = o;
+				}
+			}
+			else if (ubi == 3, 5)
+			{
+				if (F3C5 != s || F3C5 != o)
+				{
+					F3C5 = o;
+				}
+			}
+			else if (ubi == 4, 1)
+			{
+				if (F4C1 != s || F4C1 != o)
+				{
+					F4C1 = o;
+				}
+			}
+			else if (ubi == 4, 2)
+			{
+				if (F4C2 != s || F4C2 != o)
+				{
+					F4C2 = o;
+				}
+			}
+			else if (ubi == 4, 3)
+			{
+				if (F4C3 != s || F4C3 != o)
+				{
+					F4C3 = o;
+				}
+			}
+			else if (ubi == 4, 4)
+			{
+				if (F4C4 != s || F4C4 != o)
+				{
+					F4C4 = o;
+				}
+			}
+			else if (ubi == 4, 5)
+			{
+				if (F4C5 != s || F4C5 != o)
+				{
+					F4C5 = o;
+				}
+			}
+			else if (ubi == 5, 1)
+			{
+				if (F5C1 != s || F5C1 != o)
+				{
+					F5C1 = o;
+				}
+			}
+			else if (ubi == 5, 2)
+			{
+				if (F5C2 != s || F5C2 != o)
+				{
+					F5C2 = o;
+				}
+			}
+			else if (ubi == 5, 3)
+			{
+				if (F5C3 != s || F5C3 != o)
+				{
+					F5C3 = o;
+				}
+			}
+			else if (ubi == 5, 4)
+			{
+				if (F5C4 != s || F5C4 != o)
+				{
+					F5C4 = o;
+				}
+			}
+			else if (ubi == 5, 5)
+			{
+				if (F5C5 != s || F5C5 != o)
+				{
+					F5C5 = o;
+				}
 			}
 		}
 
-
-		cout << F7 << " | " << F8 << " | " << F9 << "\n" << F4 << " | " << F5 << " | " << F6 << "\n" << F1 << " | " << F2 << " | " << F3 << "\n";
+		cout << endl;
+		cout << F1C1 << " | " << F1C2 << " | " << F1C3 << " | " << F1C4 << " | " << F1C5 << " \n " << "-------------" << "\n"
+			<< F2C1 << " | " << F2C2 << " | " << F2C3 << " | " << F2C4 << " | " << F2C5 << " \n " << "---------------" << "\n"
+			<< F3C1 << " | " << F3C2 << " | " << F3C3 << " | " << F3C4 << " | " << F3C5 << " \n " << "---------------" << "\n"
+			<< F4C1 << " | " << F4C2 << " | " << F4C3 << " | " << F4C4 << " | " << F4C5 << " \n " << "---------------" << "\n"
+			<< F5C1 << " | " << F5C2 << " | " << F5C3 << " | " << F5C4 << " | " << F5C5 << endl;
 
 		i++;
-		ubi = 0;
+		
 
 		if (true)
 		{
 			tmp = jugador1;
 			jugador1 = jugador2;
 			jugador2 = tmp;
+			ubi = 0;
 		}
 		//FORMAS DE GANAR JUGADOR 1
 		//if (F1 == S && F2 == S && F3 == S)
@@ -274,44 +495,7 @@ int main()
 
 	return 0;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
-
 
 
 
