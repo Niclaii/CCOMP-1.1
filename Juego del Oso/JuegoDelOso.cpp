6464c3c1b8d20ae826b73b3d6ac7890c643f881c
Nicolas Quintanilla <107016019+Niclaii@users.noreply.github.com>
2023-03-28 17:59:26 -0500

    Add files via upload

--- Juego del Oso/JuegoDelOso.cpp
+++ Juego del Oso/JuegoDelOso.cpp
@@ -5,17 +5,16 @@ using namespace std;
 int main()
 {
 	char F1C1{}, F1C2{}, F1C3{}, F1C4{}, F1C5{}, F2C1{}, F2C2{}, F2C3{}, F2C4{}, F2C5{}, F3C1{}, F3C2{}, F3C3{}, F3C4{}, F3C5{}, F4C1{}, F4C2{}, F4C3{}, F4C4{}, F4C5{}, F5C1{}, F5C2{}, F5C3{}, F5C4{}, F5C5{};
-	int i{ 0 };
-	float ubi{ 0 };
+	int i{ 0 }, ubi1{ 0 }, ubi2{ 0 }, ptsJ1{ 0 }, ptsJ2{0};
 	bool primerapasada{ true }, jugador1{ true }, jugador2{ false }, tmp{};
-	char s{ 'S' }, o{ 'O' };
+	char s{ 'S' }, o{ 'O' },eleccion{' '};
 
 	do
 	{
 		//PRIMERA PASADA
 		if (primerapasada)
 		{
-			cout << "(1, 1)" << " | " << "(1, 2)" << " | " << "(1, 3)" << " | " << "(1, 4)" << " | " << "(1, 5)"<< " \n "<<"------------------------------------------" <<"\n"
+			std::cout << "(1, 1)" << " | " << "(1, 2)" << " | " << "(1, 3)" << " | " << "(1, 4)" << " | " << "(1, 5)"<< " \n "<<"------------------------------------------" <<"\n"
 				 << "(2, 1)" << " | " << "(2, 2)" << " | " << "(2, 3)" << " | " << "(2, 4)" << " | " << "(2, 5)" << " \n " << "------------------------------------------" << "\n" 
 				 << "(3, 1)" << " | " << "(3, 2)" << " | " << "(3, 3)" << " | " << "(3, 4)" << " | " << "(3, 5)" << " \n " << "------------------------------------------" << "\n"
 				 << "(4, 1)" << " | " << "(4, 2)" << " | " << "(4, 3)" << " | " << "(4, 4)" << " | " << "(4, 5)" << " \n " << "------------------------------------------" << "\n"
@@ -25,379 +24,430 @@ int main()
 		//TURNOS
 		if (jugador1)
 		{
-			cout << "turno de Jugador 1\n";
+			std::cout << "turno de Jugador 1\n";
 		}
 
 		if (jugador2)
 		{
-			cout << "turno de Jugador 2\n";
+			std::cout << "turno de Jugador 2\n";
 		}
+		//COMPROBANDO SI INGRESASTE S/O
+		while (true)
+		{
+			std::cout << "Elija la opcion a usar:(S,O)";
+			std::cin >> eleccion;
+			std::cout << "\n";
 
-		cout << "introduzca la ubicacion: ";
-		cin >> ubi;
+			if (eleccion == 'S' || eleccion == 's')
+			{
+				eleccion = s;
+				break;
+			}
+			else if (eleccion == 'O' || eleccion == 'o')
+			{
+				eleccion = o;
+				break;
+			}
+			else
+			{
+				std::cout << "Por favor ingrese 'S' o 'O' en mayusculas." << endl;
+			}
+		}
+		// COMPROBANDO QUE EL NUMERO INGRESADO NO PASE DE 1-5
+		while (true)
+		{
+			std::cout << "introduzca la ubicacion(Columna): ";
+			cin >> ubi1;
+			std::cout << "\n";
+
+			if (ubi1 >= 1  && ubi1 <= 5)
+			{
+				break;
+			}
+			else
+			{
+				std::cout << "Reingrese un numero entre el 1 a 5." << endl;
+			}
+
+		}
+		while (true)
+		{
+			std::cout << "introduzca la ubicacion(Fila): ";
+			cin >> ubi2;
+			std::cout << "\n";
+
+			if (ubi2 >= 1 && ubi2 <= 5)
+			{
+				break;
+			}
+			else
+			{
+				std::cout << "Reingrese un numero entre el 1 a 5." << endl;
+			}
+		}	
 
 		//JUGADOR 1
 
 		if (jugador1)
 		{
-			if (ubi == 1,1)
+			if (ubi1 == 1 && ubi2 == 1)
 			{
 				if (F1C1 != s || F1C1 != o)
-				{
-					F1C1 = s;
+				{		
+					F1C1 = eleccion;
 				}
 			}
-			else if (ubi == 1,2)
+			else if (ubi1 == 1 && ubi2 == 2)
 			{
 				if (F1C2 != s || F1C2 != o)
 				{
-					F1C2 = s;
+					F1C2 = eleccion;
 				}
 			}
-			else if (ubi == 1,3)
+			else if (ubi1 == 1 && ubi2 == 3)
 			{
 				if (F1C3 != s || F1C3 != o)
 				{
-					F1C3 = s;
+					F1C3 = eleccion;
 				}
 			}
-			else if (ubi == 1,4)
+			else if (ubi1 == 1 && ubi2 == 4)
 			{
 				if (F1C4 != s || F1C4 != o)
 				{
-					F1C4 = s;
+					F1C4 = eleccion;
 				}
 			}
-			else if (ubi == 1,5)
+			else if (ubi1 == 1 && ubi2 == 5)
 			{
 				if (F1C5 != s || F1C5 != o)
 				{
-					F1C5 = s;
+					F1C5 = eleccion;
 				}
 			}
-			else if (ubi == 2,1)
+			else if (ubi1 == 2 && ubi2 == 1)
 			{
 				if (F2C1 != s || F2C1 != o)
 				{
-					F2C1 = s;
+					F2C1 = eleccion;
 				}
 			}
-			else if (ubi == 2,2)
+			else if (ubi1 == 2 && ubi2 == 2)
 			{
 				if (F2C2 != s || F2C2 != o)
 				{
-					F2C2 = s;
+					F2C2 = eleccion;
 				}
 			}
-			else if (ubi == 2,3)
+			else if (ubi1 == 2 && ubi2 == 3)
 			{
 				if (F2C3 != s || F2C3 != o)
 				{
-					F2C3 = s;
+					F2C3 = eleccion;
 				}
 			}
-			else if (ubi == 2,4)
+			else if (ubi1 == 2 && ubi2 == 4)
 			{
 				if (F2C4 != s || F2C4 != o)
 				{
-					F2C4 = s;
+					F2C4 = eleccion;
 				}
 			}
-			else if (ubi == 2,5)
+			else if (ubi1 == 2 && ubi2 == 5)
 			{
 				if (F2C5 != s || F2C5 != o)
 				{
-					F2C5 = s;
+					F2C5 = eleccion;
 				}
 			}
-			else if (ubi == 3,1)
+			else if (ubi1 == 3 && ubi2 == 1)
 			{
 				if (F3C1 != s || F3C1 != o)
 				{
-					F3C1 = s;
+					F3C1 = eleccion;
 				}
 			}
-			else if (ubi == 3,2)
+			else if (ubi1 == 3 && ubi2 == 2)
 			{
 				if (F3C2 != s || F3C2 != o)
 				{
-					F3C2 = s;
+					F3C2 = eleccion;
 				}
 			}
-			else if (ubi == 3,3)
+			else if (ubi1 == 3 && ubi2 == 3)
 			{
 				if (F3C3 != s || F3C3 != o)
 				{
-					F3C3 = s;
+					F3C3 = eleccion;
 				}
 			}
-			else if (ubi == 3,4)
+			else if (ubi1 == 3 && ubi2 == 4)
 			{
 				if (F3C4 != s || F3C4 != o)
 				{
-					F3C4 = s;
+					F3C4 = eleccion;
 				}
 			}
-			else if (ubi == 3,5)
+			else if (ubi1 == 3 && ubi2 == 5)
 			{
 				if (F3C5 != s || F3C5 != o)
 				{
-					F3C5 = s;
+					F3C5 = eleccion;
 				}
 			}
-			else if (ubi == 4,1)
+			else if (ubi1 == 4 && ubi2 == 1)
 			{
 				if (F4C1 != s || F4C1 != o)
 				{
-					F4C1 = s;
+					F4C1 = eleccion;
 				}
 			}
-			else if (ubi == 4,2)
+			else if (ubi1 == 4 && ubi2 == 2)
 			{
 				if (F4C2 != s || F4C2 != o)
 				{
-					F4C2 = s;
+					F4C2 = eleccion;
 				}
 			}
-			else if (ubi == 4,3)
+			else if (ubi1 == 4 && ubi2 == 3)
 			{
 				if (F4C3 != s || F4C3 != o)
 				{
-					F4C3 = s;
+					F4C3 = eleccion;
 				}
 			}
-			else if (ubi == 4,4)
+			else if (ubi1 == 4 && ubi2 == 4)
 			{
 				if (F4C4 != s || F4C4 != o)
 				{
-					F4C4 = s;
+					F4C4 = eleccion;
 				}
 			}
-			else if (ubi == 4,5)
+			else if (ubi1 == 4 && ubi2 == 5)
 			{
 				if (F4C5 != s || F4C5 != o)
 				{
-					F4C5 = s;
+					F4C5 = eleccion;
 				}
 			}
-			else if (ubi == 5,1)
+			else if (ubi1 == 5 && ubi2 == 1)
 			{
 				if (F5C1 != s || F5C1 != o)
 				{
-					F5C1 = s;
+					F5C1 = eleccion;
 				}
 			}
-			else if (ubi == 5,2)
+			else if (ubi1 == 5 && ubi2 == 2)
 			{
 				if (F5C2 != s || F5C2 != o)
 				{
-					F5C2 = s;
+					F5C2 = eleccion;
 				}
 			}
-			else if (ubi == 5,3)
+			else if (ubi1 == 5 && ubi2 == 3)
 			{
 				if (F5C3 != s || F5C3 != o)
 				{
-					F5C3 = s;
+					F5C3 = eleccion;
 				}
 			}
-			else if (ubi == 5,4)
+			else if (ubi1 == 5 && ubi2 == 4)
 			{
 				if (F5C4 != s || F5C4 != o)
 				{
-					F5C4 = s;
+					F5C4 = eleccion;
 				}
 			}
-			else if (ubi == 5,5)
+			else if (ubi1 == 5 && ubi2 == 5)
 			{
 				if (F5C5 != s || F5C5 != o)
 				{
-					F5C5 = s;
+					F5C5 = eleccion;
 				}
 			}
 		}
 		//JUGADOR 2
 		if (jugador2)
 		{
-			if (ubi == 1, 1)
+			if (ubi1 == 1 && ubi2 == 1)
 			{
 				if (F1C1 != s || F1C1 != o)
 				{
-					F1C1 = o;
+					F1C1 = eleccion;
 				}
 			}
-			else if (ubi == 1, 2)
+			else if (ubi1 == 1 && ubi2 == 2)
 			{
 				if (F1C2 != s || F1C2 != o)
 				{
-					F1C2 = o;
+					F1C2 = eleccion;
 				}
 			}
-			else if (ubi == 1, 3)
+			else if (ubi1 == 1 && ubi2 == 3)
 			{
 				if (F1C3 != s || F1C3 != o)
 				{
-					F1C3 = o;
+					F1C3 = eleccion;
 				}
 			}
-			else if (ubi == 1, 4)
+			else if (ubi1 == 1 && ubi2 == 4)
 			{
 				if (F1C4 != s || F1C4 != o)
 				{
-					F1C4 = o;
+					F1C4 = eleccion;
 				}
 			}
-			else if (ubi == 1, 5)
+			else if (ubi1 == 1 && ubi2 == 5)
 			{
 				if (F1C5 != s || F1C5 != o)
 				{
-					F1C5 = o;
+					F1C5 = eleccion;
 				}
 			}
-			else if (ubi == 2, 1)
+			else if (ubi1 == 2 && ubi2 == 1)
 			{
 				if (F2C1 != s || F2C1 != o)
 				{
-					F2C1 = o;
+					F2C1 = eleccion;
 				}
 			}
-			else if (ubi == 2, 2)
+			else if (ubi1 == 2 && ubi2 == 2)
 			{
 				if (F2C2 != s || F2C2 != o)
 				{
-					F2C2 = o;
+					F2C2 = eleccion;
 				}
 			}
-			else if (ubi == 2, 3)
+			else if (ubi1 == 2 && ubi2 == 3)
 			{
 				if (F2C3 != s || F2C3 != o)
 				{
-					F2C3 = o;
+					F2C3 = eleccion;
 				}
 			}
-			else if (ubi == 2, 4)
+			else if (ubi1 == 2 && ubi2 == 4)
 			{
 				if (F2C4 != s || F2C4 != o)
 				{
-					F2C4 = o;
+					F2C4 = eleccion;
 				}
 			}
-			else if (ubi == 2, 5)
+			else if (ubi1 == 2 && ubi2 == 5)
 			{
 				if (F2C5 != s || F2C5 != o)
 				{
-					F2C5 = o;
+					F2C5 = eleccion;
 				}
 			}
-			else if (ubi == 3, 1)
+			else if (ubi1 == 3 && ubi2 == 1)
 			{
 				if (F3C1 != s || F3C1 != o)
 				{
-					F3C1 = o;
+					F3C1 = eleccion;
 				}
 			}
-			else if (ubi == 3, 2)
+			else if (ubi1 == 3 && ubi2 == 2)
 			{
 				if (F3C2 != s || F3C2 != o)
 				{
-					F3C2 = o;
+					F3C2 = eleccion;
 				}
 			}
-			else if (ubi == 3, 3)
+			else if (ubi1 == 3 && ubi2 == 3)
 			{
 				if (F3C3 != s || F3C3 != o)
 				{
-					F3C3 = o;
+					F3C3 = eleccion;
 				}
 			}
-			else if (ubi == 3, 4)
+			else if (ubi1 == 3 && ubi2 == 4)
 			{
 				if (F3C4 != s || F3C4 != o)
 				{
-					F3C4 = o;
+					F3C4 = eleccion;
 				}
 			}
-			else if (ubi == 3, 5)
+			else if (ubi1 == 3 && ubi2 == 5)
 			{
 				if (F3C5 != s || F3C5 != o)
 				{
-					F3C5 = o;
+					F3C5 = eleccion;
 				}
 			}
-			else if (ubi == 4, 1)
+			else if (ubi1 == 4 && ubi2 == 1)
 			{
 				if (F4C1 != s || F4C1 != o)
 				{
-					F4C1 = o;
+					F4C1 = eleccion;
 				}
 			}
-			else if (ubi == 4, 2)
+			else if (ubi1 == 4 && ubi2 == 2)
 			{
 				if (F4C2 != s || F4C2 != o)
 				{
-					F4C2 = o;
+					F4C2 = eleccion;
 				}
 			}
-			else if (ubi == 4, 3)
+			else if (ubi1 == 4 && ubi2 == 3)
 			{
 				if (F4C3 != s || F4C3 != o)
 				{
-					F4C3 = o;
+					F4C3 = eleccion;
 				}
 			}
-			else if (ubi == 4, 4)
+			else if (ubi1 == 4 && ubi2 == 4)
 			{
 				if (F4C4 != s || F4C4 != o)
 				{
-					F4C4 = o;
+					F4C4 = eleccion;
 				}
 			}
-			else if (ubi == 4, 5)
+			else if (ubi1 == 4 && ubi2 == 5)
 			{
 				if (F4C5 != s || F4C5 != o)
 				{
-					F4C5 = o;
+					F4C5 = eleccion;
 				}
 			}
-			else if (ubi == 5, 1)
+			else if (ubi1 == 5 && ubi2 == 1)
 			{
 				if (F5C1 != s || F5C1 != o)
 				{
-					F5C1 = o;
+					F5C1 = eleccion;
 				}
 			}
-			else if (ubi == 5, 2)
+			else if (ubi1 == 5 && ubi2 == 2)
 			{
 				if (F5C2 != s || F5C2 != o)
 				{
-					F5C2 = o;
+					F5C2 = eleccion;
 				}
 			}
-			else if (ubi == 5, 3)
+			else if (ubi1 == 5 && ubi2 == 3)
 			{
 				if (F5C3 != s || F5C3 != o)
 				{
-					F5C3 = o;
+					F5C3 = eleccion;
 				}
 			}
-			else if (ubi == 5, 4)
+			else if (ubi1 == 5 && ubi2 == 4)
 			{
 				if (F5C4 != s || F5C4 != o)
 				{
-					F5C4 = o;
+					F5C4 = eleccion;
 				}
 			}
-			else if (ubi == 5, 5)
+			else if (ubi1 == 5 && ubi2 == 5)
 			{
 				if (F5C5 != s || F5C5 != o)
 				{
-					F5C5 = o;
+					F5C5 = eleccion;
 				}
 			}
 		}
 
-		cout << endl;
-		cout << F1C1 << " | " << F1C2 << " | " << F1C3 << " | " << F1C4 << " | " << F1C5 << " \n " << "-------------" << "\n"
+		std::cout << endl;
+		std::cout << F1C1 << " | " << F1C2 << " | " << F1C3 << " | " << F1C4 << " | " << F1C5 << " \n " << "-------------" << "\n"
 			<< F2C1 << " | " << F2C2 << " | " << F2C3 << " | " << F2C4 << " | " << F2C5 << " \n " << "---------------" << "\n"
 			<< F3C1 << " | " << F3C2 << " | " << F3C3 << " | " << F3C4 << " | " << F3C5 << " \n " << "---------------" << "\n"
 			<< F4C1 << " | " << F4C2 << " | " << F4C3 << " | " << F4C4 << " | " << F4C5 << " \n " << "---------------" << "\n"
@@ -405,13 +455,15 @@ int main()
 
 		i++;
 		
-
+		//ROTAR TURNOS Y REINICIAR VARIABLES
 		if (true)
 		{
 			tmp = jugador1;
 			jugador1 = jugador2;
 			jugador2 = tmp;
-			ubi = 0;
+			ubi1 = 0;
+			ubi2 = 0;
+			eleccion = ' ';
 		}
 		//FORMAS DE GANAR JUGADOR 1
 		//if (F1 == S && F2 == S && F3 == S)
