36609beff241c47672a06a230727f71dde51bb06
Gabo <107078570+GaboE2@users.noreply.github.com>
2023-06-10 09:44:33 -0500

    Update cNPC.h

--- Pokemon/cNPC.h
+++ Pokemon/cNPC.h
@@ -1,17 +1,16 @@
 #pragma once
-#include <iostream>
-#include "cPersonaje.h"
 
+#include "Personaje.h"
+#include <string>
 
-class NPC :public Personaje
-{
-	private:
-	
-	
-	
-	public:
+class NPC : public Personaje {
+private:
+    std::string Sprite_mundo;
+    std::string Nombre;
+    std::string Sprite_Combate;
+    std::string Dialogo_Interaccion;
 
-
-
-
-};
+public:
+    void Asignar_Sprite(std::string sprite);
+    void Asignar_Nombre(std::string nombre);
+    void Equipo_pokemon();
