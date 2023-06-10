d380971dd9873d5731d406e85b58c9e765939739
Gabo <107078570+GaboE2@users.noreply.github.com>
2023-06-10 09:47:18 -0500

    Create NPC.cpp

---
+++ Pokemon/NPC.cpp
@@ -0,0 +1,14 @@
+#include "NPC.h"
+
+void NPC::Asignar_Sprite(std::string sprite) {
+    Sprite_mundo = sprite;
+    Sprite_Combate = sprite;
+}
+
+void NPC::Asignar_Nombre(std::string nombre) {
+    Nombre = nombre;
+}
+
+void NPC::Equipo_pokemon() { // eq pk
+    
+}
