a6eaadc41937f16856eff2be0d9535b2fb90de3e
Nicolas Quintanilla <107016019+Niclaii@users.noreply.github.com>
2023-06-19 23:12:47 -0500

    Delete Pokemon directory

--- Pokemon/Personaje.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "cPersonaje.h"
-#include <string>
-
-void Personaje::Asignar_Sprite(std::string sprite , std::string a)
-{
-    Sprite_Mundo = sprite;
-    Sprite_Combate = sprite;
-}
